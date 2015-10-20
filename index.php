ddb1b9efff8d1bc16692d1c683dec7a2d24b9148
Nico Staple <nico.staple@gmail.com>
2015-10-20 10:51:14 -0400

    update google analytics and remove extra ;s

--- index.php
+++ index.php
@@ -26,18 +26,14 @@ define('SYSROOT',dirname(__FILE__));
     <![endif]-->
     <script type="text/javascript" src="//use.typekit.net/xto8xnn.js"></script>
     <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
-    <script type="text/javascript">
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-      var _gaq = _gaq || [];
-      _gaq.push(['_setAccount', 'UA-28679304-1']);
-      _gaq.push(['_setDomainName', 'nicostaple.com']);
-      _gaq.push(['_trackPageview']);
-
-      (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-      })();
+      ga('create', 'UA-28679304-9', 'auto');
+      ga('send', 'pageview');
 
     </script>
   </head>
@@ -71,10 +67,10 @@ define('SYSROOT',dirname(__FILE__));
     
     <!-- Backbone templates -->
     <script id="stations-list-template" type="text/template">
-      <?php require_once(SYSROOT."/js/templates/stations-list.js"); ?>;
+      <?php require_once(SYSROOT."/js/templates/stations-list.js"); ?>
     </script>
     <script id="station-counter-template" type="text/template">
-      <?php require_once(SYSROOT."/js/templates/station-counter.js"); ?>;
+      <?php require_once(SYSROOT."/js/templates/station-counter.js"); ?>
     </script>
 
     <script>
